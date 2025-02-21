d427d2cdaa9ee308e58587a4145bf89aef2c90bf
Aswin562005 <balamuruganaswin17@gmail.com>
2025-02-22 00:19:10 +0530

    Adding : Some Design

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<style>
+    header 
+    {
+        background-color: rgb(146, 21, 17);
+        color: #fff;
+        text-align: center;
+        padding: 20px 10px;
+        box-shadow: 0 2px 5px rgba(0,0,0,0.3);
+    }
+    header h1 {
+        margin: 0;
+        font-size: 4em;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+    }
+    header p {
+        margin: 5px 0 0;
+        font-size: 1.5em;
+    }
+</style>
+<header>
+    <h1 style=color:yellow>N.M.S.S.V.N. College</h1>
+     <p>Empowering Students for a Brighter Future</p>
+ </header>
